a32b7a7c121aefd3c37a492d4b46ee479e354485
xiaodoujiang <1426703092@qq.com>
2024-03-27 10:16:55 +0800

    LC206_ReverseList

--- src/com/bmilk/algorithm/list/LC206_ReverseList.java
+++ src/com/bmilk/algorithm/list/LC206_ReverseList.java
@@ -3,11 +3,17 @@ package com.bmilk.algorithm.list;
 public class LC206_ReverseList {
     public static void main(String[] args) {
         LC206_ReverseList o = new LC206_ReverseList();
-        ListNode node2 = new ListNode(2);
+        ListNode node5 = new ListNode(5);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+
+        ListNode node2 = new ListNode(2, node3);
         ListNode node1 = new ListNode(1,node2);
-        ListNode node = o.reverseList(node1);
+        // ListNode node = o.reverseList(node1);
+        ListNode node = o.reverseList_withLoop(node1);
         while (null != node){
             System.out.println(node.val);
+            node= node.next;
         }
     }
 
@@ -29,6 +35,26 @@ public class LC206_ReverseList {
         reverseList(node.next, tail);
         node.next.next = node;
     }
+
+
+    public ListNode reverseList_withLoop(ListNode head) {
+        if(null == head || null == head.next){
+            return head;
+        }
+        ListNode p1 = head;
+        ListNode p2 = p1.next;
+        ListNode p3 = p2.next;
+        p1.next = null;
+        p2.next = p1;
+        while (null != p3){
+            p1 = p2;
+            p2 = p3;
+            p3 = p3.next;
+            p2.next = p1;
+        }
+        return p2;
+    }
+
 
     static class ListNode{
         private int val;
