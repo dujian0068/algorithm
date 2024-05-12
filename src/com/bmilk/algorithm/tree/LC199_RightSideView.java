dc20f4102c1c496358d4926624a71272cd4403af
xiaodoujiang <1426703092@qq.com>
2024-05-12 15:13:10 +0800

    LC199_RightSideView

---
+++ src/com/bmilk/algorithm/tree/LC199_RightSideView.java
@@ -0,0 +1,65 @@
+package com.bmilk.algorithm.tree;
+
+import java.util.*;
+
+public class LC199_RightSideView {
+
+    Map<Integer, TreeNode> nodeMap = new HashMap<>();
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        dfs(root, 1);
+        if(nodeMap.isEmpty()) return result;
+        for (int i =1; null != nodeMap.get(i); i++){
+            result.add(nodeMap.get(i).val);
+        }
+        return result;
+
+    }
+
+    public void dfs(TreeNode root, int deepth){
+        if(root == null ) return;
+        nodeMap.putIfAbsent(deepth, root);
+        dfs(root.right, deepth+1);
+        dfs(root.left, deepth+1);
+
+    }
+
+    public List<Integer> bfs(TreeNode root){
+        List<Integer> result = new ArrayList<>();
+        if(null == root){
+            return result;
+        }
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        while (!nodeQueue.isEmpty()){
+            int size = nodeQueue.size();
+            result.add(nodeQueue.peek().val);
+            for (int i = 0; i< size; i++){
+                TreeNode poll = nodeQueue.poll();
+                if(null != poll.right){
+                    nodeQueue.add(poll.right);
+                }
+                if(null != poll.left){
+                    nodeQueue.add(poll.left);
+                }
+            }
+        }
+        return result;
+    }
+
+    static class TreeNode{
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){}
+        TreeNode(int val){ this.val = val;}
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
