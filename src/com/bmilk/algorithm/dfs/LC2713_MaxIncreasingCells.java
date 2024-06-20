725dbe83a2903902ba50e1cf9a3b5bd64b68226b
dujian0068 <1426703092@qq.com>
2024-06-20 17:29:25 +0800

    LC2713_MaxIncreasingCells

--- src/com/bmilk/algorithm/dfs/LC2713_MaxIncreasingCells.java
+++ src/com/bmilk/algorithm/dfs/LC2713_MaxIncreasingCells.java
@@ -30,6 +30,7 @@ public class LC2713_MaxIncreasingCells {
                 arr[arrI++] = new int[]{i, j};
             }
         }
+        // 从小到大排序 状态转转移一定是从最小的开始
         Arrays.sort(arr, (o1, o2) -> {
             int i = mat[o1[0]][o1[1]] - mat[o2[0]][o2[1]];
             return i;
@@ -47,12 +48,16 @@ public class LC2713_MaxIncreasingCells {
         int x = arr[i][0];
         int y = arr[i][1];
         dp[x][y] = 1;
+        // 如果当前值比他做在行的最大的严格递增的位置都大，那么可以从这个位置转移至当前位置
         if (mat[x][row[x]] < mat[x][y]) {
             dp[x][y] = Math.max(dp[x][y], dp[x][row[x]] + 1);
         }
+        // 如果当前值比他做在列的最大的严格递增的位置都大，那么可以从这个位置转移至当前位置
         if (mat[col[y]][y] < mat[x][y]) {
             dp[x][y] = Math.max(dp[x][y], dp[col[y]][y] + 1);
         }
+        // 有可能有与当前数相同的数，他们的转移需要从上一个比他严格小的位置转移
+        // 之所以先转移这些值而不是先更新行、列最大值是因为防止下一个值和当前值在同行或者同列，这样转移就会异常
         if (i < len - 1 && mat[x][y] == mat[arr[i + 1][0]][arr[i + 1][1]]) {
             i = doDP(i + 1, mat, len);
         }
